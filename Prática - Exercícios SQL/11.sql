572ea5a72686c933337fa167bc9b2716b604c140
matheussvini <matheussvini@outlook.com>
2022-12-20 09:45:25 -0300

    feat: exercise

---
+++ Prática - Exercícios SQL/11.sql
@@ -0,0 +1,7 @@
+SELECT * FROM clientes WHERE nome ='Martinha Lima Zambujal';
+
+SELECT * FROM produtos WHERE nome = 'Fone Topo de Linha';
+
+SELECT * FROM compras WHERE (id_cliente = 5 AND id_produto = 14);
+
+DELETE FROM compras WHERE id=6;
